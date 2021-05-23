e558d31d340ce186c37d7560e169872abbd835e3
Vipansh Thakur <vipansh@github.com>
2021-05-24 02:13:21 +0530

    pagination bug fixed

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,31 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import Filters from "../components/filters/Filters";
 import Header from "../components/header/Header";
 import FlightDetails from "../Modals/FlightDetails";
 import { Table } from "../components/table/Table";
-import { ApiCallProvider } from "../context/ApiCallContext";
+import { ApiCallContext } from "../context/ApiCallContext";
 import Pagination from "../components/filters/Pagination";
 
 import { FlightDetailsProvider } from "../context/OpenFlightDetailsContext";
 
 const HomePage = () => {
- 
+  const { allData } = useContext(ApiCallContext);
 
   return (
     <div>
-      <ApiCallProvider>
-        <FlightDetailsProvider>
-          <FlightDetails />
-          <Header />
-          <div className="mx-auto">
-            <div className="flex justify-end items-center my-2 w-full">
-              <Filters />
-            </div>
-            <Table />
-            <Pagination />
+      <FlightDetailsProvider>
+        <FlightDetails />
+        <Header />
+        <div className="mx-auto">
+          <div className="flex justify-end items-center my-2 w-full">
+            <Filters />
           </div>
-        </FlightDetailsProvider>
-      </ApiCallProvider>
+          <Table />
+          {allData ? allData.length > 0 && <Pagination /> : null}
+        </div>
+      </FlightDetailsProvider>
     </div>
   );
 };
